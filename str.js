413c087efc7e05fd0ce85a825f0b4df16d0739c0
Divyanshugit2006 <2025umt1278@mnit.ac.in>
2026-08-13 18:35:18 +0530

    Add files via upload

---
+++ str.js
@@ -0,0 +1,78 @@
+let he = document.querySelector("h3");
+let user = [];
+let game = [];
+
+let buttons = ["box1", "box2", "box3", "box4"];
+
+let level = 0;
+let start = false;
+
+document.addEventListener("keypress", () => {
+    document.querySelector("body").style.backgroundColor="white";    
+    if (start == false) {
+        start = true;
+        levelup();
+    }
+});
+
+function btnflash(btn) {
+    btn.classList.add("flash");
+    setTimeout(() => {
+        btn.classList.remove("flash")
+    }, 500);
+}
+
+function checkans(idx) {
+    if (user[idx] === game[idx]) {
+        if (user.length == game.length) {
+            setTimeout(levelup, 1000);
+        }
+    }
+    else {
+        he.innerHTML = `Game Over!<br>Your Score: ${level - 1}<br>Press any key to restart`;
+        document.querySelector("body").style.backgroundColor="red";
+        reset();
+    }
+}
+
+function reset() {
+    start = false;
+    game = [];
+    user = [];
+    level = 0;
+}
+
+function btnuserflash(btn) {
+    btn.classList.add("userflash");
+    setTimeout(() => {
+        btn.classList.remove("userflash")
+    }, 500);
+
+    let usercolor = btn.getAttribute("id");
+    user.push(usercolor);
+    // console.log(user);
+    checkans(user.length - 1);
+}
+
+function levelup() {
+    user = [];
+    level++;
+    he.innerText = `Level ${level}`;
+    let index = Math.floor(Math.random() * 4);
+    let randcolor = buttons[index];
+    game.push(randcolor);
+    let btn = document.querySelector(`.${randcolor}`)
+    console.log(game);
+    btnflash(btn);
+
+}
+
+let allbox = document.querySelectorAll(".box");
+for (let box of allbox) {
+    box.addEventListener("click", () => {
+        if (start) {
+            btnuserflash(box);
+
+        }
+    })
+}
